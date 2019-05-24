8749b095a8330f82db16b4329aba2b5888818f94
Neil Bickle (AMBCS) (AssocIScT) (TMIET) (ICTTech) <43986316+NeilBickle@users.noreply.github.com>
2019-05-24 10:43:51 +0100

    Ver 1

---
+++ main.pas
@@ -0,0 +1,25 @@
+
+
+
+program Number;
+uses Crt;
+Var
+Num : Integer;
+sum : Integer;
+result : Integer;
+begin
+ clrscr;
+ Num := 0;
+ write('Simple Number Doubler');
+ writeln; writeln;
+ write('Enter One Number : ');
+ readln(Num);
+sum := (Num * 2);
+result := sum;
+ writeln;
+ writeln;
+ Write('The Answer Is ' ,result,'.');
+ readln;
+end.
+
+
